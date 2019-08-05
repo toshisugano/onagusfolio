6105bb97fe03ba3b59f08b19304a5e43e22cd538
Gregory Sugano <onagususa@gmail.com>
2019-08-05 00:28:41 -0400

    rev

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');   
 const app = express();
       router = express.Router();  
-//const passport = require('passport');
-//const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
     
 
-//const keys = require('./config/keys');
+const keys = require('./config/keys');
 
 /*passport.use(new GoogleStrategy({ 
     clientID: keys.clientID,
@@ -22,6 +22,9 @@ const port = process.env.PORT || 8000;
 app.use('/styles', express.static(__dirname + '/styles'));
 app.use('/images', express.static(__dirname + '/images'));
 app.use('/scripts', express.static(__dirname + '/scripts')); 
+app.use('/login/styles', express.static(__dirname + '/styles'));
+app.use('/login/scripts', express.static(__dirname + '/scripts'));
+app.use('/login/images', express.static(__dirname + '/images'));
 app.use('/projects/styles', express.static(__dirname + '/styles')); 
 app.use('/projects/scripts', express.static(__dirname + '/scripts'));  
 app.use('/projects/images', express.static(__dirname + '/images')); 
@@ -30,6 +33,8 @@ app.get('/' , function(req, res ){
     res.sendFile(__dirname + '/home.html');
 });
 
+
+//Projects
 app.get('/projects/leechesposter' , function(req, res ){ 
     res.sendFile(__dirname + '/projects/leechesposter/index.html');
 }); 
@@ -44,21 +49,41 @@ app.get('/projects/sccperformance' , function(req, res ){
 
 app.get('/projects/sccperformancesite' , function(req, res ){ 
     res.sendFile(__dirname + '/projects/sccperformancesite/index.html');
-}); 
-
+});  
 
 app.get('/projects/leeches' , function(req, res ){ 
     res.sendFile(__dirname + '/projects/leeches/index.html');
 }); 
- 
+
+
+//Develop 
 app.get('/develop' , function(req, res ){ 
     res.sendFile(__dirname + '/dev.html');
 }); 
 
+//Design
 app.get('/design' , function(req, res ){ 
     res.sendFile(__dirname + '/home.html');
 }); 
 
-console.log("running");
+//Oauth
+app.get('/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+// Login 
+app.get('/login/user', (req, res) => { 
+    res.sendFile(__dirname + '/login.html');
+});
+ 
+console.log("running at port : " + port );
 
 app.listen(port);
